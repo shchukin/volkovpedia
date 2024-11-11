c6d90cffe4678a8e618aacc79508a5aacc54ce22
Anton Shchukin <a.a.shchukin@gmail.com>
2024-11-11 22:28:02 +0700

    footer expanding

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,42 +1,48 @@
 (function ($) {
 
-    /* Select placeholder */
-    function selectPlaceholder($element) {
-        if ($element.val() === 'placeholder') {
-            $element.parent('.input').addClass('input--placeholder-is-chosen');
-        } else {
-            $element.parent('.input').removeClass('input--placeholder-is-chosen');
-        }
-    }
+    // /* Select placeholder */
+    // function selectPlaceholder($element) {
+    //     if ($element.val() === 'placeholder') {
+    //         $element.parent('.input').addClass('input--placeholder-is-chosen');
+    //     } else {
+    //         $element.parent('.input').removeClass('input--placeholder-is-chosen');
+    //     }
+    // }
+    //
+    // $('select.input__widget').each(function () {
+    //     selectPlaceholder($(this));
+    // }).on('change', function () {
+    //     selectPlaceholder($(this));
+    // });
+    //
+    // /* Expanding textarea */
+    // function expandTextarea($element) {
+    //     $element.css('height', 'auto');
+    //     $element.css('height', ($element[0].scrollHeight + 2 * parseInt($element.css('border-width'), 10)) + 'px');
+    // }
+    //
+    // $('.input--expandable .input__widget').each(function () {
+    //     expandTextarea($(this));
+    // }).on('input', function () {
+    //     expandTextarea($(this));
+    // });
+    //
+    // /* Error field */
+    // $('.input__widget').on('focus', function () {
+    //     $(this).parents('.input').removeClass('input--error');
+    //     $(this).parents('.input').nextUntil(':not(.helper--error)').remove();
+    // });
+    //
+    //
+    // /* Init mask */
+    // $('[type="tel"]').inputmask({
+    //     alias: 'phoneru',
+    // });
 
-    $('select.input__widget').each(function () {
-        selectPlaceholder($(this));
-    }).on('change', function () {
-        selectPlaceholder($(this));
-    });
+    document.querySelectorAll('.footer__handler').forEach(item => item.addEventListener('click', function(event) {
+        event.preventDefault();
+        item.closest('.footer__section').classList.toggle('footer__section--expanded');
+    }));
 
-    /* Expanding textarea */
-    function expandTextarea($element) {
-        $element.css('height', 'auto');
-        $element.css('height', ($element[0].scrollHeight + 2 * parseInt($element.css('border-width'), 10)) + 'px');
-    }
-
-    $('.input--expandable .input__widget').each(function () {
-        expandTextarea($(this));
-    }).on('input', function () {
-        expandTextarea($(this));
-    });
-
-    /* Error field */
-    $('.input__widget').on('focus', function () {
-        $(this).parents('.input').removeClass('input--error');
-        $(this).parents('.input').nextUntil(':not(.helper--error)').remove();
-    });
-
-
-    /* Init mask */
-    $('[type="tel"]').inputmask({
-        alias: 'phoneru',
-    });
 
 })(jQuery);
