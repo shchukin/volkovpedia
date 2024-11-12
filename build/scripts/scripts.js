8803d0a393a198c22e50c94f4d1e51c33c252e46
Anton Shchukin <a.a.shchukin@gmail.com>
2024-11-12 20:00:53 +0700

    build

--- build/scripts/scripts.js
+++ build/scripts/scripts.js
@@ -1,42 +1,91 @@
 (function ($) {
 
-    /* Select placeholder */
-    function selectPlaceholder($element) {
-        if ($element.val() === 'placeholder') {
-            $element.parent('.input').addClass('input--placeholder-is-chosen');
-        } else {
-            $element.parent('.input').removeClass('input--placeholder-is-chosen');
+    // /* Select placeholder */
+    // function selectPlaceholder($element) {
+    //     if ($element.val() === 'placeholder') {
+    //         $element.parent('.input').addClass('input--placeholder-is-chosen');
+    //     } else {
+    //         $element.parent('.input').removeClass('input--placeholder-is-chosen');
+    //     }
+    // }
+    //
+    // $('select.input__widget').each(function () {
+    //     selectPlaceholder($(this));
+    // }).on('change', function () {
+    //     selectPlaceholder($(this));
+    // });
+    //
+    // /* Expanding textarea */
+    // function expandTextarea($element) {
+    //     $element.css('height', 'auto');
+    //     $element.css('height', ($element[0].scrollHeight + 2 * parseInt($element.css('border-width'), 10)) + 'px');
+    // }
+    //
+    // $('.input--expandable .input__widget').each(function () {
+    //     expandTextarea($(this));
+    // }).on('input', function () {
+    //     expandTextarea($(this));
+    // });
+    //
+    // /* Error field */
+    // $('.input__widget').on('focus', function () {
+    //     $(this).parents('.input').removeClass('input--error');
+    //     $(this).parents('.input').nextUntil(':not(.helper--error)').remove();
+    // });
+    //
+    //
+    // /* Init mask */
+    // $('[type="tel"]').inputmask({
+    //     alias: 'phoneru',
+    // });
+
+    const $html = document.querySelector('html');
+
+
+
+    /* Расхлопывание меню (актуально на смартфонах) */
+
+    const $menu = document.querySelector('.menu');
+    const $burger = document.querySelector('.burger');
+
+    document.querySelector('.burger').addEventListener('click', function(event) {
+        $html.classList.toggle('burger-expanded');
+    });
+
+    document.querySelector('.menu__close').addEventListener('click', function(event) {
+        $html.classList.remove('burger-expanded');
+    });
+
+    document.addEventListener('keydown', (event) => {
+        if (event.key === 'Escape') {
+            $html.classList.remove('burger-expanded');
         }
-    }
-
-    $('select.input__widget').each(function () {
-        selectPlaceholder($(this));
-    }).on('change', function () {
-        selectPlaceholder($(this));
     });
 
-    /* Expanding textarea */
-    function expandTextarea($element) {
-        $element.css('height', 'auto');
-        $element.css('height', ($element[0].scrollHeight + 2 * parseInt($element.css('border-width'), 10)) + 'px');
-    }
-
-    $('.input--expandable .input__widget').each(function () {
-        expandTextarea($(this));
-    }).on('input', function () {
-        expandTextarea($(this));
-    });
-
-    /* Error field */
-    $('.input__widget').on('focus', function () {
-        $(this).parents('.input').removeClass('input--error');
-        $(this).parents('.input').nextUntil(':not(.helper--error)').remove();
+    document.addEventListener('click', (event) => {
+        if (!$menu.contains(event.target) && !$burger.contains(event.target)) {
+            $html.classList.remove('burger-expanded');
+        }
     });
 
 
-    /* Init mask */
-    $('[type="tel"]').inputmask({
-        alias: 'phoneru',
+    /* Расхлопывание поиска (актуально на смартфонах) */
+
+    const $findField = document.querySelector('.find__field');
+
+    document.querySelector('.find__handler').addEventListener('click', function(event) {
+        $html.classList.toggle('search-expanded');
+        if ($html.classList.contains('search-expanded')) {
+            $findField.focus();
+        }
     });
+
+
+    /* Расхлопывание подменюшек (актуально на смартфонах) */
+
+    document.querySelectorAll('.nav__handler').forEach(item => item.addEventListener('click', function(event) {
+        event.preventDefault();
+        item.closest('.nav__section').classList.toggle('footer__section--expanded');
+    }));
 
 })(jQuery);
